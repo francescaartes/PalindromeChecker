e9de64eb6ca5cbf071fdac202d6c303eac70e663
Francesca Artes <122511777+francescaartes@users.noreply.github.com>
2024-03-13 14:33:12 +0800

    checkPalindrome

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# PalindromeChecker
- A web app that checks whether a given word is palindrome. freeCodeCamp JavaScript Algorithms and Data Structures 1 of 5 Certification Projects
+# Palindrome Checker
+
+This project is a simple web application that allows users to check if a given input text is a palindrome or not. A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward (ignoring spaces, punctuation, and capitalization).
+
+https://francescaartes.github.io/PalindromeChecker/
--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const input = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
 const result = document.getElementById('result');
+let filterInput;
 
-checkBtn.addEventListener('click', () => {
-    result.classList.remove('show');
-    let filterInput = input.value.replace(/[^A-Za-z0-9]/ig, "").toLowerCase();
-    if (input.value === "") {
-        return alert('Please input a value')
-    }
-    const reverseInput = filterInput.split("").reverse().join("");
+checkBtn.addEventListener('click', checkPalindrome)
+input.addEventListener('keypress', checkPalindrome)
 
-    if (reverseInput == filterInput) {
-        result.innerHTML = `<span>'${input.value}'</span> is a palindrome!`
-        return result.classList.add('show');
+input.addEventListener('keyup', () => {
+    filterInput = input.value.replace(/[^A-Za-z0-9]/ig, "").toLowerCase();
+    
+    if (filterInput) {
+        checkBtn.classList.add('active');
     } else {
-        result.innerHTML = `<span>'${input.value}'</span> is a NOT palindrome!`
-        return result.classList.add('show');
+        checkBtn.classList.remove('active');
+        result.classList.remove('show');
     }
 })
+
+function checkPalindrome(event) {
+    if (event.key === 'Enter' || event.type === 'click') {
+        const reverseInput = filterInput.split("").reverse().join("");
+
+        if (reverseInput == filterInput) {
+            result.innerHTML = `<span class="palindrome">${input.value}</span> is a palindrome!`
+            return result.classList.add('show');
+        } else {
+            result.innerHTML = `<span class="not-palindrome">${input.value}</span> is a not palindrome!`
+            return result.classList.add('show');
+        }
+    }
+}
